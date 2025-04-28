c116f103e39fc98fbb2f4724d516863853180641
malopez23 <malopez.rodriguez23@gmail.com>
2025-04-28 16:34:12 -0300

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './global.css'
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-6 rounded-lg shadow-lg text-center mt-10 w-4/5 mx-auto"> 
+      <h1 className="text-3xl font-bold mb-4">Testando TailwindCSS no Regency Agenda</h1>
+      <p className="text-lg">Se você está vendo um fundo azul, texto branco e bordas arredondadas, o TailwindCSS está funcionando!</p>
+    </div>
+  );
+}
+
+export default App;
